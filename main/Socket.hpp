ab2f5b8cf77f146f831b6aed6edf1b10471ec97c
Yeham Kim <yekim@c5r2s7.42seoul.kr>
2021-06-25 16:31:53 +0900

    refactor: socket 클래스 수정

--- main/Socket.hpp
+++ main/Socket.hpp
@@ -6,13 +6,12 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <cstring>
-#include <poll.h>
 #include "ErrorHandler.hpp"
+#include "NginxConfig.hpp"
 #include <iostream>
 
 class Socket {
     protected:
-        struct pollfd _pollfd;
         struct sockaddr_in _socketAddr;
         int _socket;
         socklen_t _socketLen;
@@ -20,12 +19,9 @@ class Socket {
     public:
         Socket(int socket_);
         virtual ~Socket();
-        void setPollFd(int events_);
-        void setPollFd(struct pollfd pollfd_);
         int getSocket() const;
-        struct pollfd getPollFd() const;
 
         virtual int runSocket() = 0;
 };
-std::ostream &operator<<(std::ostream &out, Socket& socket_);
+
 #endif
