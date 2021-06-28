74658f4d69535f2a198933dceddeca9652632648
yamkim <coramdeo0708@gmail.com>
2021-06-28 22:13:00 +0900

    feat: bind 오류를 수정하기 위해 setsockopt 추가

--- main/ListeningSocket.hpp
+++ main/ListeningSocket.hpp
@@ -3,11 +3,13 @@
 
 #include "Socket.hpp"
 
+#define OPTVAL_BUFFER_SIZE 1024
 class ListeningSocket : public Socket {
     private:
         int _backlog;
         int _portNum;
         const char* _ip;
+        char _optval[OPTVAL_BUFFER_SIZE];
         ListeningSocket();
 
     public:
