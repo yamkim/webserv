96aa88ec3f641174e085c24f74e666b9fc47c140
joohongpark <daleaderprofq@gmail.com>
2021-07-10 16:59:37 +0900

    refactor: main 코드 주석 간결하게 정돈

--- main/main.cpp
+++ main/main.cpp
@@ -38,28 +38,24 @@ int main(int argc, char *argv[])
                     Socket* instance = reinterpret_cast<Socket*>(kq.getInstance(i));
                     long data = kq.getData(i);
                     if (dynamic_cast<KernelQueue::PairQueue*>(instance) != NULL) {
-                        // NOTE: CGI Event 발생
+                        // NOTE: PairQueue 이벤트 처리
                         KernelQueue::PairQueue* pairQueue = reinterpret_cast<KernelQueue::PairQueue*>(instance);
                         pairQueue->stopSlave();
                     } else if (dynamic_cast<ListeningSocket*>(instance) != NULL) {
-                        // NOTE: Listening Socket Event 발생
-                        // NOTE: 부하 테스트를 진행하며 여러 요청이 올 때 한번에 여러 연결을 생성하도록 하였습니다. 해당 구간에서 다음과 같이 수정함으로 성능 개선을 확인하였습니다.
+                        // NOTE: Listening Socket 이벤트 처리
                         for (long i = 0; i < data; i++) {
                             ConnectionSocket* cSocket = new ConnectionSocket(instance->getSocket(), instance->getConfig(), nginxConfig);
                             timer.addObj(cSocket, std::atoi(instance->getConfig().dirMap["keepalive_timeout"].c_str()));
                             kq.addReadEvent(cSocket->getSocket(), reinterpret_cast<void*>(cSocket));
                         }
                     } else if (dynamic_cast<ConnectionSocket*>(instance) != NULL) {
-                        // NOTE: Connection Socket Event 발생
+                        // NOTE: Connection Socket 이벤트 처리
                         ConnectionSocket* cSocket = dynamic_cast<ConnectionSocket*>(instance);
                         cSocket->setDynamicBufferSize(data);
                         if (kq.isClose(i)) {
-                            // NOTE: 연결 종료 여부 확인
                             timer.delObj(cSocket, ConnectionSocket::ConnectionSocketKiller);
-                            //delete cSocket;
                         } else if (kq.isReadEvent(i)) {
                             if (cSocket->HTTPRequestProcess() == HTTPRequestHandler::FINISH) {
-                                // NOTE: to Write Event
                                 kq.modEventToWriteEvent(i);
                             }
                         } else if (kq.isWriteEvent(i)) {
@@ -67,7 +63,6 @@ int main(int argc, char *argv[])
                             if (phase == HTTPResponseHandler::FINISH) {
                                 kq.deletePairEvent(i);
                                 timer.delObj(cSocket, ConnectionSocket::ConnectionSocketKiller);
-                                //delete cSocket;
                             } else if (phase == HTTPResponseHandler::CGI_REQ) {
                                 kq.setPairEvent(i, cSocket->getCGIfd());
                             }
@@ -78,7 +73,7 @@ int main(int argc, char *argv[])
             timer.CheckTimer(ConnectionSocket::ConnectionSocketKiller);
         }
     } catch (const std::exception& error) {
-        std::cout << error.what() << std::endl;
+        std::cerr << error.what() << std::endl;
         return (1);
     }
     return (0);
