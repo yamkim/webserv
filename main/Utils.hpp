0130f21656503d7ba4246dfcc3e9109ea1dd1f9e
joohongpark <daleaderprofq@gmail.com>
2021-07-14 14:45:03 +0900

    fix: chunked로 req body가 날라갈 때 Cgi에서 옥텟스트림으로 받게함

--- main/Utils.hpp
+++ main/Utils.hpp
@@ -4,11 +4,13 @@
 //#include "NginxConfig.hpp"
 #include <string>
 #include <map>
+#include <sstream>
 
 class Utils {
     public:
         static std::string getMapValue(std::map<std::string, std::string> map_, const std::string& key);
         static std::string randomStringGenerator(int length);
         static int hextoint(std::string str);
+        static std::string ltos(long number);
 };
 #endif
