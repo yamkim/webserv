46d7ca353b9e408b217d70aaf069543252811cfa
yamkim <coramdeo0708@gmail.com>
2021-07-14 12:00:48 +0900

    refactor: allwoed_method가 있는 경우 처리부 위치 변경
    - allowed_method 확인부 process에서 setFileInDirectory로 이동

--- main/HTTPRequestHandler.hpp
+++ main/HTTPRequestHandler.hpp
@@ -7,31 +7,28 @@
 #include "HTTPHandler.hpp"
 #include "FileController.hpp"
 #include "Parser.hpp"
+#include "Utils.hpp"
 #include <iostream>
 
 class HTTPRequestHandler : public HTTPHandler {
     public:
         HTTPRequestHandler(int connectionFd, const NginxConfig::ServerBlock& serverConf, NginxConfig::NginxConfig& nginxConf);
         virtual ~HTTPRequestHandler();
-        typedef enum e_Phase {
-            PARSE_STARTLINE, 
-            PARSE_HEADER, 
-            PARSE_BODY, 
-            FINISH
-        } Phase;
-        virtual HTTPRequestHandler::Phase process(HTTPData& data);
+        typedef enum e_Phase {PARSE_STARTLINE, PARSE_HEADER, PARSE_BODY_NBYTES, PARSE_BODY_CHUNK, REMOVE_CRNF, FINISH} Phase;
+        virtual HTTPRequestHandler::Phase process(HTTPData& data, long bufferSize);
     private:
         bool getStartLine(HTTPData& data);
         std::string _requestLine;
         Phase _phase;
         long _contentLength;
+        long _contentLengthSum;
+        bool _chunkFinish;
+        long _dynamicBufferSize;
         FileController* _fileController;
         HTTPRequestHandler();
 
         bool getHeader(void);
-        void showHeader(void);
-        int findNewLine(const char *buffer);
-        bool setHeaderString(void);
+        std::string* getDataByCRNF(void);
         void setMethod(std::string method);
         void setURI(std::string URI);
         void setProtocol(std::string protocol);
--- main/HTTPResponseHandler.cpp
+++ main/HTTPResponseHandler.cpp
@@ -45,7 +45,7 @@ std::string HTTPResponseHandler::getErrorPage(const std::string& absPath, const 
     std::vector<std::string>::const_iterator iter;
     if (!serverErrorPageVec.empty()) {
         iter = serverErrorPageVec.end() - 1;
-        // std::cout << "[DEBUG] ERROR Page Search[ser]: " << absPath + *iter << std::endl;
+        std::cout << "[DEBUG] ERROR Page Search[ser]: " << absPath + *iter << std::endl;
         if (FileController::checkType(absPath + *iter) == FileController::FILE) {
            _errorPageList  = serverErrorPageVec;
             serverErrorPage = *iter;
@@ -61,7 +61,7 @@ std::string HTTPResponseHandler::getErrorPage(const std::string& absPath, const 
     std::string locErrorPage;
     if (!locErrorPageVec.empty()) {     // loc error page가 있는 경우
         iter = locErrorPageVec.end() - 1;
-        // std::cout << "[DEBUG] ERROR Page Search[loc]: " << absPath + *iter << std::endl;
+        std::cout << "[DEBUG] ERROR Page Search[loc]: " << absPath + *iter << std::endl;
         if (FileController::checkType(absPath + *iter) == FileController::FILE) {
             _errorPageList = locErrorPageVec;
             locErrorPage = *iter;
@@ -135,7 +135,9 @@ void HTTPResponseHandler::setHTMLHeader(const HTTPData& data) {
 
 void HTTPResponseHandler::showResponseInformation(HTTPData &data) {
     std::cout << "Response Information=============================" << std::endl;
+    std::cout << "# Typed Request URL by client: https://127.0.0.1:4242" << data._reqURI << std::endl;
     std::cout << "# root + Request URL: " << data._root + data._reqURI << std::endl;
+    std::cout << "# Request Method: " << data._reqMethod << std::endl;
     std::cout << "# Request URL: " << data._reqURI << std::endl;
     std::cout << "# Status Code: " << data._statusCode << std::endl;
     std::cout << "# Absolute File Path: " << data._resAbsoluteFilePath << std::endl;
@@ -193,32 +195,39 @@ HTTPResponseHandler::Phase HTTPResponseHandler::setInformation(HTTPData& data, i
         }
     }
     if (!_errorPage.empty() && isErrorPageList(data._statusCode, _errorPageList)) {
+        // 경로상의 error_page를 띄우면 200 아니였는가..?
         return setInformation(data, 200, data._resAbsoluteFilePath + _errorPage);
     }
     return GET_STATIC_HTML;
 }
 
 HTTPResponseHandler::Phase HTTPResponseHandler::setFileInDirectory(HTTPData& data, const std::string& absLocPath) {
-    if (!_indexPage.empty()) { // index file이 어떻게든 있는 경우
-        // index 파일이 서버 컴퓨터에 있는지 판별
-        FileController::Type indexType = FileController::checkType(absLocPath + _indexPage);
-        if (indexType == FileController::FILE) {
-            _phase = setInformation(data, 200, absLocPath + _indexPage);
-        } else {
-            // _phase = setInformation(data, 403, absLocPath);
-            _phase = setInformation(data, 404, absLocPath);
-        }
-    } else {                  // index file이 어디에도 설정되지 않은 경우
-        _locConf.dirMap["autoindex"] = _locConf.dirMap["autoindex"].empty()
-                                    ? _serverConf.dirMap["autoindex"]
-                                    : _locConf.dirMap["autoindex"];
-        if (_locConf.dirMap["autoindex"] == "on") {
-            data._statusCode = 200;
-            setGeneralHeader(data);
-            data._URIExtension = "html";
-            _phase = GET_STATIC_HTML;
-        } else {
-            _phase = setInformation(data, 403, absLocPath);
+    if (!_locConf.allowed_method.empty() 
+        && find(_locConf.allowed_method.begin(), _locConf.allowed_method.end(), data._reqMethod) == _locConf.allowed_method.end()) {
+        return setInformation(data, 405, data._root + "/");
+    } else {
+        if (!_indexPage.empty()) { // index file이 어떻게든 있는 경우
+            // index 파일이 서버 컴퓨터에 있는지 판별
+            FileController::Type indexType = FileController::checkType(absLocPath + _indexPage);
+            if (indexType == FileController::FILE) {
+                _phase = setInformation(data, 200, absLocPath + _indexPage);
+            } else {
+                // _phase = setInformation(data, 403, absLocPath);
+                _phase = setInformation(data, 404, absLocPath);
+            }
+        } else {                  // index file이 어디에도 설정되지 않은 경우
+            _locConf.dirMap["autoindex"] = _locConf.dirMap["autoindex"].empty()
+                                        ? _serverConf.dirMap["autoindex"]
+                                        : _locConf.dirMap["autoindex"];
+            if (_locConf.dirMap["autoindex"] == "on") {
+                _phase = setInformation(data, 200, "");
+                data._statusCode = 200;
+                setGeneralHeader(data);
+                data._URIExtension = "html";
+                _phase = GET_STATIC_HTML;
+            } else {
+                _phase = setInformation(data, 403, absLocPath);
+            }
         }
     }
     return _phase;
@@ -249,16 +258,17 @@ HTTPResponseHandler::Phase HTTPResponseHandler::process(HTTPData& data, long buf
         _locConf = getMatchingLocationConfiguration(data);
         // TODO: root에 따라서 변하는 경우, 처리할지 말지 고민: location block 내에도 root가 올 수 있음
         if (!_locConf._locationPath.empty()) { // 4
-            if (!_locConf.allowed_method.empty() 
-                && find(_locConf.allowed_method.begin(), _locConf.allowed_method.end(), data._reqMethod) == _locConf.allowed_method.end()) {
-                _phase = setInformation(data, 405, "");
-            } else {
-                // index page 세팅 및 error page 세팅
-                std::string tmpAbsPath = _locConf._locationPath == "/" ? "/" : _locConf._locationPath + "/";
-                _indexPage = getIndexPage(data._root + tmpAbsPath, _serverConf.index, _locConf.index);
-                _errorPage = getErrorPage(data._root + tmpAbsPath, _serverConf.error_page, _locConf.error_page);
-                // 만약 location context 내부에 root가 존재한다면, data._root + _locConf._locationPath가 됨.
-                data._root = _locConf.dirMap["root"].empty() ? data._root : _locConf.dirMap["root"];
+            // index page 세팅 및 error page 세팅
+            std::string tmpAbsPath = _locConf._locationPath == "/" ? "/" : _locConf._locationPath + "/";
+            _indexPage = getIndexPage(data._root + tmpAbsPath, _serverConf.index, _locConf.index);
+            _errorPage = getErrorPage(data._root + tmpAbsPath, _serverConf.error_page, _locConf.error_page);
+            // 만약 location context 내부에 root가 존재한다면, data._root + _locConf._locationPath가 됨.
+            data._root = _locConf.dirMap["root"].empty() ? data._root : _locConf.dirMap["root"];
+            // if (!_locConf.allowed_method.empty() 
+            //     && find(_locConf.allowed_method.begin(), _locConf.allowed_method.end(), data._reqMethod) == _locConf.allowed_method.end()) {
+            //     std::cout << "[DEBUG] 405 ERROR PAGE========================";
+            //     _phase = setInformation(data, 405, data._root + "/");
+            // } else {
                 _type = FileController::checkType(data._root + data._URIFilePath);
                 if (_type == FileController::DIRECTORY) {
                     if (data._URIFilePath[data._URIFilePath.size() - 1] != '/') {
@@ -285,7 +295,6 @@ HTTPResponseHandler::Phase HTTPResponseHandler::process(HTTPData& data, long buf
                     
                     _type = FileController::checkType(tmpLocPath);
                     if (_type == FileController::DIRECTORY) {
-                        // 여긴 무조건 '/' 붙어 오겠지?!
                         if (data._URIFilePath[data._URIFilePath.size() - 1] != '/') {
                             _phase = setInformation(data, 301, data._URIFilePath + "/");
                         } else {
@@ -301,7 +310,7 @@ HTTPResponseHandler::Phase HTTPResponseHandler::process(HTTPData& data, long buf
                         _phase = setInformation(data, 404, data._root + "/");
                     }
                 }
-            }
+            // }
         } else { // TODO: locatioin에 대한 정보가 없는 경우 어떻게 처리할건지 고려 => root가 있으면 root에서 index 파일을 찾아야됨.
             _phase = setInformation(data, 404, data._root + _locConf._locationPath + "/");
         }
