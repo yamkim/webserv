83f07419fe19a79aa919aab2163ce3f7bd7a6136
joohongpark <daleaderprofq@gmail.com>
2021-07-20 16:57:12 +0900

    feat: res 단에도 try-catch문 추가하여 종료 방지

--- main/ConnectionSocket.cpp
+++ main/ConnectionSocket.cpp
@@ -66,22 +66,28 @@ void ConnectionSocket::setConnectionData(struct sockaddr_in _serverSocketAddr, s
 
 HTTPResponseHandler::Phase ConnectionSocket::HTTPResponseProcess(void) {
     HTTPResponseHandler::Phase phase;
-    phase = _res->process(*_data, getDynamicBufferSize());
-    if (phase == HTTPResponseHandler::CGI_RUN) {
-        // NOTE: CGI일 때 길이가 리턴되면 연결을 끊으면 안될거 같은데...
+    try {
+        phase = _res->process(*_data, getDynamicBufferSize());
+    } catch (const std::exception& error) {
+        std::cerr << error.what() << std::endl;
         _connectionCloseByServer = true;
-    } else if (phase == HTTPResponseHandler::FINISH) {
-        if (_connectionCloseByServer == false) {
-            delete _req;
-            delete _res;
-            delete _data;
-            _req = new HTTPRequestHandler(_socket, _serverConf, _nginxConf);
-            _res = NULL;
-            _data = new HTTPData();
-            setConnectionData(_clientAddr, _serverAddr);
-            phase = HTTPResponseHandler::FINISH_RE;
-        }
+        phase = HTTPResponseHandler::FINISH;
     }
+        if (phase == HTTPResponseHandler::CGI_RUN) {
+            // NOTE: CGI일 때 길이가 리턴되면 연결을 끊으면 안될거 같은데...
+            _connectionCloseByServer = true;
+        } else if (phase == HTTPResponseHandler::FINISH) {
+            if (_connectionCloseByServer == false) {
+                delete _req;
+                delete _res;
+                delete _data;
+                _req = new HTTPRequestHandler(_socket, _serverConf, _nginxConf);
+                _res = NULL;
+                _data = new HTTPData();
+                setConnectionData(_clientAddr, _serverAddr);
+                phase = HTTPResponseHandler::FINISH_RE;
+            }
+        }
     return (phase);
 }
 
