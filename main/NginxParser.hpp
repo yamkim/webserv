2d28a706aa11905315e152d1e5302bf5c45fed27
yamkim <coramdeo0708@gmail.com>
2021-07-21 10:24:29 +0900

    fix: nginx.conf 에러처리 보강

--- main/NginxParser.hpp
+++ main/NginxParser.hpp
@@ -6,7 +6,7 @@
 class NginxParser : public Parser {
     public:        
         NginxParser(const std::string& fileName);
-        void skipComment(const std::string& str, std::size_t &commentPos);
+        static void skipComment(const std::string& str, std::size_t &commentPos);
         static void findBlockSet(const std::string& buf, std::stack<int>st, std::vector<std::pair<std::string, std::size_t> >& vec, std::size_t& pos);
         static std::string getBlockContent(const std::string& buf, std::size_t& pos);
 };
